6b742127408f2eee0dfb1f3790ad2e68d2904d88
mwestera <matthijs.westera@gmail.com>
2023-03-23 16:04:27 +0100

    refactoring

--- adventures/sentiment.py
+++ adventures/sentiment.py
@@ -4,6 +4,8 @@ from sklearn import linear_model
 from sklearn.metrics import precision_score, recall_score, f1_score
 import pandas
 
+import wordcloud
+import matplotlib.pyplot as plt
 
 """
 The result of part 2 of our slightly rushed bag-of-words adventure; next class will start with a recap and some
@@ -20,7 +22,7 @@ the preprocessing script sentiment_to_jsonl.py . After that, you can run the cur
 negative_words = ['awful', 'disgusting', 'shit', 'horrible', 'terrible']
 positive_words = ['wonderful', 'amazing', 'beautiful', 'great', 'lovely']
 
-def basic_classifier(text):
+def handcrafted_model(text):
 
     tokenized_text = text.split()
 
@@ -40,66 +42,61 @@ def count_words_from_list(text, target_words):
     return sum([tokenized_text.count(target_word) for target_word in target_words])
 
 
-def train_slightly_more_advanced_classifier(train):
+def train_model(X, y):
     """
     Returns a logistic regression model using the features n_pos and n_neg.
     """
     model = linear_model.LogisticRegression()
-    model.fit(train[['n_pos', 'n_neg']], train['sentiment'])
+    model.fit(X, y)
 
     return model
 
 
-def train_even_more_advanced_classifier(train):
-    """
-    Returns a logistic regression model using the features n_pos and n_neg.
-    """
-    model = linear_model.LogisticRegression()
-    model.fit(train[word_features], train['sentiment'])
-
-    return model
-
-
-def evaluate(predictions, targets):
+def evaluate(y_pred, target_y):
     return {
-        'prec': precision_score(targets, predictions, pos_label='pos'),
-        'rec': recall_score(targets, predictions, pos_label='pos'),
-        'f1': f1_score(targets, predictions, pos_label='pos'),
+        'prec': precision_score(target_y, y_pred, pos_label='pos'),
+        'rec': recall_score(target_y, y_pred, pos_label='pos'),
+        'f1': f1_score(target_y, y_pred, pos_label='pos'),
     }
 
 
-reviews = pandas.read_json('data/sentiment.jsonl', lines=True)
-
-reviews['n_pos'] = [count_words_from_list(text, positive_words) for text in reviews['text']]
-reviews['n_neg'] = [count_words_from_list(text, negative_words) for text in reviews['text']]
-
 word_features = []
-for word in positive_words + negative_words:
-    reviews[f'n_{word}'] = [text.count(word) for text in reviews['text']]
-    word_features.append(f'n_{word}')
+
+def add_features(reviews):
+    reviews['n_pos'] = [count_words_from_list(text, positive_words) for text in reviews['text']]
+    reviews['n_neg'] = [count_words_from_list(text, negative_words) for text in reviews['text']]
+    for word in positive_words + negative_words:
+        reviews[f'n_{word}'] = [text.count(word) for text in reviews['text']]
+        word_features.append(f'n_{word}')
 
 
-train, test = train_test_split(reviews)
+def main():
 
-test['basic_model_prediction'] = [basic_classifier(text) for text in test['text']]
+    reviews = pandas.read_json('data/sentiment.jsonl', lines=True)
+    add_features(reviews)
+    train, test = train_test_split(reviews)
 
-sma_model = train_slightly_more_advanced_classifier(train)
+    logreg_posneg = train_model(train[['n_pos', 'n_neg']], train['sentiment'])
+    logreg_wordlists = train_model(train[word_features], train['sentiment'])
 
-test['sma_model_prediction'] = sma_model.predict(test[['n_pos', 'n_neg']])
+    test['handcrafted_prediction'] = [handcrafted_model(text) for text in test['text']]
+    test['posneg_prediction'] = logreg_posneg.predict(test[['n_pos', 'n_neg']])
+    test['wordlists_prediction'] = logreg_wordlists.predict(test[[f'n_{word}' for word in positive_words + negative_words]])
 
-ema_model = train_even_more_advanced_classifier(train)
+    print(test.head(5).to_string())
 
-test['ema_model_prediction'] = ema_model.predict(test[[f'n_{word}' for word in positive_words + negative_words]])
+    basic_model_scores = evaluate(test['handcrafted_prediction'], test['sentiment'])
+    sma_model_scores = evaluate(test['posneg_prediction'], test['sentiment'])
+    ema_model_scores = evaluate(test['wordlists_prediction'], test['sentiment'])
 
-print(test.head(5).to_string())
+    print(basic_model_scores)
+    print(sma_model_scores)
+    print(ema_model_scores)
 
-basic_model_scores = evaluate(test['basic_model_prediction'], test['sentiment'])
-sma_model_scores = evaluate(test['sma_model_prediction'], test['sentiment'])
-ema_model_scores = evaluate(test['ema_model_prediction'], test['sentiment'])
+    for word, coefficient in zip(positive_words + negative_words, logreg_wordlists.coef_.squeeze()):
+        print(word, coefficient)
 
-print(basic_model_scores)
-print(sma_model_scores)
-print(ema_model_scores)
 
-for word, coefficient in zip(positive_words + negative_words, ema_model.coef_.squeeze()[1:]):
-    print(word, coefficient)
+
+if __name__ == '__main__':
+    main()
